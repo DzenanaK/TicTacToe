c481b3eade6c654f35de7c19774867526454f51e
DzenanaK <64224921+DzenanaK@users.noreply.github.com>
2025-01-03 21:52:01 +0100

    Update ConstVariables.hpp

--- include/ConstVariables.hpp
+++ include/ConstVariables.hpp
@@ -5,7 +5,7 @@ namespace TTT {
 
 inline constexpr uint8_t COL = 3;  // Acceptable values: COL >= 3
 inline constexpr uint8_t ROW = 3;  // Acceptable values: ROW >=3
-inline constexpr uint8_t END_GAME_POSITION = -1;
+inline constexpr int END_GAME_POSITION = -1;
 inline constexpr char DEF_VAL = 'Q';
 
 }  // namespace TTT
